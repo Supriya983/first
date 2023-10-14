e5c0f46500f26861fbbbb2ea1a991f6678a163ff
Supriya983 <142827107+Supriya983@users.noreply.github.com>
2023-10-14 14:44:52 +0530

    Create programs.cpp
    started coding 2nd day :-)

---
+++ programs.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+ // cout<<"hello world";
+//return 0;
+//}
+
+//second program (program to add two numbers)
+int first_number , second_number , sum;
+
+cout<< "enter two integers: ";
+cin>>first_number >> second_number;
+
+//sum of two numbers is stored in variable sum of two numbers
+sum = first_number + second_number;
+
+//print sum
+cout<< first_number << " + " << second_number << " = " << sum;
+
+return0;
+}
